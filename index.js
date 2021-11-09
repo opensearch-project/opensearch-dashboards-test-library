15ffe07a8761af713b7ad886e87f9d1fd7e4f9de
Tianle Huang <60111637+tianleh@users.noreply.github.com>
2021-11-08 19:56:18 -0800

    remove unused sample js (#7)
    * remove unused sample js
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * bump version
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-export { hello } from "./page-object-model/sample.js"
-
 export { HomePage } from './page-object-model/home-page/home-page.js'
 
 export { HOME_PAGE_ELEMENTS } from './page-object-model/home-page/page-elements.js'
